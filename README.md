3e5357fbff776abe20b8f041e36563a346081d95
Alistair Willis <alistairrwillis@gmail.com>
2017-12-18 11:16:02 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+•	api.openweathermap.org/data/2.5/weather?q=London&APPID=ba02317420a71b482c575129ae75f584
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app" class="app">
+
+
+    <div class="content">
+      <header>
+        <h1>Weather App</h1>
+      </header>
+    </div>
+
+
+    <footer>
+      <p class="footer-author">Coded by Alistair Willis</p>
+    </footer>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+
+
+</style>
