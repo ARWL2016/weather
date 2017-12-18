dd03ae022b7397bb4b127b8b62723a57df16f2fd
Alistair Willis <alistairrwillis@gmail.com>
2017-12-18 12:14:13 +0900

    add form

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,29 @@
       <header>
         <h1>Weather App</h1>
       </header>
+      <main role="content">
+        <form>
+          <div class="form-label-wrapper">
+            <label for="locationSearch" class="form-label">Location</label>
+            <span class="form-required">required</span>
+          </div>
+          <input
+            type="text"
+            id="locationSearch"
+            placeholder="type a location to search"
+            class="form-input"
+            v-model="searchTerm"
+            required>
+          <input
+            type="submit"
+            value="Search"
+            class="form-btn">
+        </form>
+        <section class="result">
+          {{searchTerm}}
+        </section>
+      </main>
+
     </div>
 
 
@@ -22,14 +45,11 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      searchTerm: ''
     }
   }
 }
 </script>
 
-<style lang="scss">
 
-
-
-</style>
